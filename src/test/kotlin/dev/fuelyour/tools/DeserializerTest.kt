d66782f883031a052960d70ec743a90bb91b13c3
Trevor Young <trevor@fuelyour.dev>
2019-11-02 02:21:45 -0600

    Added test for getTypeForParamAt that tests getting generic types

--- src/test/kotlin/dev/fuelyour/tools/DeserializerTest.kt
+++ src/test/kotlin/dev/fuelyour/tools/DeserializerTest.kt
@@ -7,11 +7,14 @@ import io.kotlintest.shouldBe
 import io.kotlintest.shouldThrow
 import io.kotlintest.specs.StringSpec
 import io.kotlintest.tables.row
+import java.lang.reflect.ParameterizedType
 import kotlin.reflect.full.declaredFunctions
 
 fun topLevelFunction(param1: Long, param2: Float) { }
+fun topLevelListFunction(param1: List<Long>, param2: List<Float>) { }
 
 fun String.extensionFunction(param1: Int) { }
+fun List<String>.extensionListFunction(param1: List<Int>) { }
 
 class TestClass(val param1: String, val param2: Int) {
   companion object {
@@ -20,8 +23,19 @@ class TestClass(val param1: String, val param2: Int) {
   fun classFunction(param1: Double, param2: Boolean) { }
 }
 
+class TestListClass(val param1: List<String>, val param2: List<Int>) {
+  companion object {
+    fun companionListFunction(param1: List<Short>) { }
+  }
+  fun classListFunction(param1: List<Double>, param2: List<Boolean>) { }
+}
+
 object Singleton {
   fun singletonFunction(param1: Char, param2: Byte) { }
+}
+
+object ListSingleton {
+  fun singletonListFunction(param1: List<Char>, param2: List<Byte>) { }
 }
 
 class DeserializerTest : Deserializer by DeserializerImpl(), StringSpec() {
@@ -74,6 +88,43 @@ class DeserializerTest : Deserializer by DeserializerImpl(), StringSpec() {
         shouldThrow<IndexOutOfBoundsException> {
           method.getTypeForParamAt(paramIndex)
         }
+      }
+    }
+
+    val listKclass = TestListClass::class
+    val listCtor = listKclass.constructors.first()
+    val listClassFun = listKclass.declaredFunctions.first()
+    val listTopLevelFun = ::topLevelListFunction
+    val listCompanionFun = TestListClass.Companion::companionListFunction
+    val listSingletonFun = ListSingleton::singletonListFunction
+    val listExtensionFun = List<String>::extensionListFunction
+
+    "getTypeForParamAt can get the type of lists" {
+      forall(
+        row(listCtor, 0, String::class.java),
+        row(listCtor, 1, Int::class.javaObjectType),
+
+        // For class functions, the first parameter is an instance
+        // of the class
+        row(listClassFun, 1, Double::class.javaObjectType),
+        row(listClassFun, 2, Boolean::class.javaObjectType),
+
+        // For extension functions, the first parameter is an instance
+        // of the class being extended
+        row(listExtensionFun, 0, String::class.java),
+        row(listExtensionFun, 1, Int::class.javaObjectType),
+
+        row(listTopLevelFun, 0, Long::class.javaObjectType),
+        row(listTopLevelFun, 1, Float::class.javaObjectType),
+
+        row(listCompanionFun, 0, Short::class.javaObjectType),
+
+        row(listSingletonFun, 0, Char::class.javaObjectType),
+        row(listSingletonFun, 1, Byte::class.javaObjectType)
+      ) { method, paramIndex, listType ->
+        val type = method.getTypeForParamAt(paramIndex) as ParameterizedType
+        type.rawType shouldBe List::class.java
+        type.actualTypeArguments[0] shouldBe listType
       }
     }
   }
