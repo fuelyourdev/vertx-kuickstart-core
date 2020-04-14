b7355c790327c8410602ba3d495cb57951ed4191
Trevor Young <trevor@fuelyour.dev>
2020-04-14 02:31:56 -0600

    Modified gradle config for publishing to sonatype.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,29 @@
 import org.gradle.jvm.tasks.Jar
 
+group = "dev.fuelyour"
+version = "0.0.1-SNAPSHOT"
+description = "Core libraries used by microservices created from the vertx-kuickstart template"
+extra["isReleaseVersion"] = !version.toString().endsWith("SNAPSHOT")
+
 plugins {
   kotlin("jvm") version "1.3.50"
   id("org.jetbrains.dokka") version "0.10.1"
+  signing
   `maven-publish`
 }
 
-group = "dev.fuelyour"
-version = "0.0.1-SNAPSHOT"
+buildscript {
+  repositories {
+    mavenLocal()
+    jcenter()
+    mavenCentral()
+  }
+}
 
 repositories {
-  mavenCentral()
+  mavenLocal()
   jcenter()
+  mavenCentral()
 }
 
 val vertxVersion = "3.8.1"
@@ -59,28 +71,68 @@ tasks {
 
 val sourcesJar by tasks.creating(Jar::class) {
   dependsOn(JavaPlugin.CLASSES_TASK_NAME)
-  classifier = "sources"
+  archiveClassifier.set("sources")
   from(sourceSets["main"].allSource)
 }
 
 val dokkaJar by tasks.creating(Jar::class) {
   group = JavaBasePlugin.DOCUMENTATION_GROUP
   description = "Assembles Kotlin docs with Dokka"
-  classifier = "javadoc"
+  archiveClassifier.set("javadoc")
   from(tasks.dokka)
 }
 
 publishing {
   publications {
-    create<MavenPublication>("default") {
+    create<MavenPublication>("mavenJava") {
       from(components["java"])
       artifact(sourcesJar)
       artifact(dokkaJar)
+      pom {
+        name.set("${project.group}:${rootProject.name}")
+        description.set("Core libraries used by microservices created from the vertx-kuickstart template")
+        url.set("https://github.com/fuelyourdev/vertx-kuickstart-core")
+        licenses {
+          license {
+            name.set("MIT License")
+            url.set("http://www.opensource.org/licenses/mit-license.php")
+          }
+        }
+        developers {
+          developer {
+            id.set("fuelyourdev")
+            name.set("Trevor Young")
+            email.set("trevor@fuelyour.dev")
+            organization.set("Fuel Your Dev, LLC")
+            organizationUrl.set("https://fuelyour.dev")
+          }
+        }
+        scm {
+          connection.set("scm:git:git://github.com/fuelyourdev/vertx-kuickstart-core.git")
+          developerConnection.set("scm:git:git://github.com/fuelyourdev/vertx-kuickstart-core.git")
+          url.set("https://github.com/fuelyourdev/vertx-kuickstart-core/tree/master")
+        }
+      }
     }
   }
   repositories {
     maven {
-      url = uri("$buildDir/repository")
+      val publishUrl = if (rootProject.extra["isReleaseVersion"] as Boolean) {
+        "https://oss.sonatype.org/service/local/staging/deploy/maven2"
+      } else {
+        "https://oss.sonatype.org/content/repositories/snapshots"
+      }
+      url = uri(publishUrl)
+      credentials {
+        username = extra["ossrhUsername"] as String
+        password = extra["ossrhPassword"] as String
+      }
     }
+  }
+}
+
+if (rootProject.extra["isReleaseVersion"] as Boolean && gradle.taskGraph.hasTask("publish")) {
+  signing {
+    sign(publishing.publications["mavenJava"])
   }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,6 @@
 rootProject.name = "vertx-kuickstart-core"
 
+val ossrhUsername: String by settings
+val ossrhPassword: String by settings
+extra["ossrhUsername"] = ossrhUsername
+extra["ossrhPassword"] = ossrhPassword
