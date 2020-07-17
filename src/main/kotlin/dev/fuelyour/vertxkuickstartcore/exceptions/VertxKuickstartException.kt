fd363dfae6cc729e92227079d79625119e037b82
Trevor Young <trevor@fuelyour.dev>
2020-07-17 01:10:21 -0600

    Made multiple improvements to exception handling

--- src/main/kotlin/dev/fuelyour/vertxkuickstartcore/exceptions/VertxKuickstartException.kt
+++ src/main/kotlin/dev/fuelyour/vertxkuickstartcore/exceptions/VertxKuickstartException.kt
@@ -1,10 +1,15 @@
 package dev.fuelyour.vertxkuickstartcore.exceptions
 
+import java.lang.RuntimeException
+
 /**
  * Base exception for vertx-quickstart-core libraries to inform users of the core libraries of misuse of the core
  * libraries.
  */
-class VertxKuickstartException : Exception {
+class VertxKuickstartException : RuntimeException {
+
+    override val message: String
+        get() = super.message ?: ""
 
     constructor(message: String) : super(message)
 
