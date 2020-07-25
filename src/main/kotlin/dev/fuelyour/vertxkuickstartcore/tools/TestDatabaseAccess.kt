b74eaf5d8553f7f7f761011bbed93bcde6700419
Trevor Young <trevor@fuelyour.dev>
2020-07-25 00:56:26 -0600

    Added tools to support database testing

--- src/main/kotlin/dev/fuelyour/vertxkuickstartcore/tools/TestDatabaseAccess.kt
+++ src/main/kotlin/dev/fuelyour/vertxkuickstartcore/tools/TestDatabaseAccess.kt
@@ -1,41 +1,40 @@
 package dev.fuelyour.vertxkuickstartcore.tools
 
+import dev.fuelyour.vertxkuickstartcore.config.config
 import dev.fuelyour.vertxkuickstartcore.exceptions.ModelNotFoundException
+import dev.fuelyour.vertxkuickstartcore.migrations.migrate
 import io.vertx.core.Vertx
 import io.vertx.core.json.JsonObject
 import io.vertx.kotlin.pgclient.pgConnectOptionsOf
 import io.vertx.kotlin.sqlclient.getConnectionAwait
 import io.vertx.kotlin.sqlclient.poolOptionsOf
+import io.vertx.kotlin.sqlclient.preparedQueryAwait
+import io.vertx.pgclient.PgException
 import io.vertx.pgclient.PgPool
 import io.vertx.sqlclient.SqlClient
 
-interface DatabaseAccess {
-    /**
-     * Get a connection to the database.
-     *
-     * @param dbAction code block in which a connection to the database is
-     * available
-     */
-    suspend fun <T : Any> getConnection(dbAction: suspend (SqlClient) -> T?): T
+object TestDatabaseAccess : DatabaseAccess {
+    private var pool: PgPool? = null
 
-    /**
-     * Get a transactional connection to the database. On success the
-     * transaction is committed. On error the transaction is rolled back.
-     *
-     * @param dbAction code block in which a connection to the database is
-     * available
-     */
-    suspend fun <T : Any> getTransaction(dbAction: suspend (SqlClient) -> T?): T
-}
+    private suspend fun init() {
+        val vertx = Vertx.vertx()
+        val config = vertx.config()
+        val testDBName = "${config.getString("SERVICE_DB_NAME")}_test"
+        val testConfig = config.copy().put("SERVICE_DB_NAME", testDBName)
+        initTestPool(testConfig, vertx)
+        try {
+            getConnection { }
+        } catch (e: PgException) {
+            println("Creating database $testDBName")
+            val da = DatabaseAccessImpl(config, vertx)
+            da.getConnection { conn ->
+                conn.preparedQueryAwait("CREATE DATABASE $testDBName;")
+            }
+        }
+        migrate(testConfig)
+    }
 
-/**
- * Establishes a connection to the database specified in the config,
- * and maintains a connection pool to that database.
- */
-class DatabaseAccessImpl(config: JsonObject, vertx: Vertx) : DatabaseAccess {
-    private val pool: PgPool
-
-    init {
+    private fun initTestPool(config: JsonObject, vertx: Vertx) {
         val connectionOptions = pgConnectOptionsOf(
             port = config.getInteger("SERVICE_DB_PORT"),
             host = config.getString("SERVICE_DB_HOST"),
@@ -56,14 +55,20 @@ class DatabaseAccessImpl(config: JsonObject, vertx: Vertx) : DatabaseAccess {
     override suspend fun <T : Any> getConnection(
         dbAction: suspend (SqlClient) -> T?
     ): T {
+        if (pool == null) {
+            init()
+        }
+        val pool = pool ?: throw Exception("Failed to init connection pool")
         val result: T?
         val connection = pool.getConnectionAwait()
+        val transaction = connection.begin()
         try {
             result = dbAction.invoke(connection)
         } catch (ex: Exception) {
             throw ex
         } finally {
             try {
+                transaction.rollback()
                 connection.close()
             } catch (ignore: Exception) {
             }
@@ -76,23 +81,6 @@ class DatabaseAccessImpl(config: JsonObject, vertx: Vertx) : DatabaseAccess {
     override suspend fun <T : Any> getTransaction(
         dbAction: suspend (SqlClient) -> T?
     ): T {
-        val result: T?
-        val connection = pool.getConnectionAwait()
-        val transaction = connection.begin()
-        try {
-            result = dbAction.invoke(connection)
-            transaction.commit()
-        } catch (ex: Exception) {
-            transaction.rollback()
-            throw ex
-        } finally {
-            try {
-                connection.close()
-            } catch (ignore: Exception) {
-            }
-        }
-        if (result == null)
-            throw ModelNotFoundException("Record not found")
-        return result
+        return getConnection(dbAction)
     }
 }
